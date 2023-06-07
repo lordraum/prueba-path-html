b21367c2a64190893f2cded889c33ce0afded925
David Gomez <lordraum@gmail.com>
2023-06-07 11:34:13 -0500

    First commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+      },
+    },
+  },
+  base: "/project/",
+});
